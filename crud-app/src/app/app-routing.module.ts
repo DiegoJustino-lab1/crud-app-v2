e9aee2e6051ffce8f6e7c4938e754d0d7fa4efcb
Diego Justino <diegojsilva01@outlook.com>
2024-05-04 11:04:50 -0300

    sabado_primeiro

--- crud-app/src/app/app-routing.module.ts
+++ crud-app/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { UsersComponent } from './pages/home/components/users/users.component';
-import { WelcomeComponent } from './pages/home/components/welcome/welcome.component';
-import { TodoListComponent } from './pages/home/pages/todo-list/todo-list.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { NavHeaderComponent } from './pages/home/components/nav-header/nav-header.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './pages/adduser/adduser.component';
 import { EditUserComponent } from './pages/edituser/edituser.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoListComponent } from './pages/home/pages/todo-list/todo-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NavHeaderComponent } from './shared/components/nav-header/nav-header.component';
+import { WelcomeComponent } from './shared/components/welcome/welcome.component';
+import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "/login", pathMatch: 'full'},
@@ -18,11 +18,11 @@ const routes: Routes = [
   {path: "add-user", component: AddUserComponent},
   {path: "edit-user", component: EditUserComponent},
   {path: "app", component: HomeComponent},
+  {path: "users", component: UsersComponent},
   { path: 'nav-header', component: NavHeaderComponent },  {
     path: "app", component: HomeComponent,
     children: [
       {path: "", component: WelcomeComponent},
-      {path: "users", component: UsersComponent},
       {path: "todo-list", component: TodoListComponent}
     ]
   }
--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './pages/adduser/adduser.component';
 import { EditUserComponent } from './pages/edituser/edituser.component';
-import { NavHeaderComponent } from './pages/home/components/nav-header/nav-header.component';
-import { NavbarComponent } from './pages/home/components/navbar/navbar.component';
-import { UsersComponent } from './pages/home/components/users/users.component';
-import { WelcomeComponent } from './pages/home/components/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TodoListComponent } from './pages/home/pages/todo-list/todo-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { WelcomeComponent } from './shared/components/welcome/welcome.component';
+import { NavHeaderComponent } from './shared/components/nav-header/nav-header.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
